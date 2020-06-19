055395380ef97f55b82e696a8dc6e1fc8e294ca5
James Ward <james@jamesward.com>
2020-06-19 10:33:59 -0600

    try this

--- conf/scripts/shutdown-on-docker-exit.sh
+++ conf/scripts/shutdown-on-docker-exit.sh
@@ -3,9 +3,11 @@
 while true; do
   uptime=$(cat /proc/uptime | awk '{printf "%0.f", $1}')
 
-  num_docker_ps=$(docker ps | grep -v stackdriver-logging-agent | tail -n +2 | wc -l)
+  num_all_docker_ps=$(docker ps | tail -n +2 | wc -l)
 
-  if ((num_docker_ps == "0")) && ((uptime > 30)); then
+  num_user_docker_ps=$(docker ps | grep -v stackdriver-logging-agent | tail -n +2 | wc -l)
+
+  if ((num_all_docker_ps == "1")) && ((num_user_docker_ps == "0")) && ((uptime > 60)); then
     echo "no docker processes running, so shutting down"
     poweroff
   fi
